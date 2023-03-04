7f707b16ff1bad5a7c5d8d940427cc91e8a21903
Paul Dieringer <pauldieringer@gmail.com>
2023-03-04 00:58:12 -0700

    First commit!

---
+++ ericHelp.py
@@ -0,0 +1,21 @@
+
+
+def sum(x, y):
+    output = int(x) + int(y) 
+    print(output) 
+    
+
+def calculate(x, y, operand):
+    if(operand == "Add"):
+        output = int(x) + int(y) 
+
+    print(output) 
+
+
+firstInt = input("Enter a number: ")
+secondInt = input("Enter a number: ")
+operandInput = input("Enter a operation: ")
+
+
+sum(firstInt, secondInt)
+calculate(firstInt, secondInt, operandInput)
